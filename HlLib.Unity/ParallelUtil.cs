f776938b79118ce9c9f45e4c8c195ee0cadf1726
hal1932 <yu.arai.19@gmail.com>
2016-05-12 01:03:47 +0900

    Parallel系の簡易実装を追加

---
+++ HlLib.Unity/ParallelUtil.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+namespace HlLib.Unity
+{
+    public static class ParallelUtil
+    {
+        public static void For(int fromInclusive, int toExclusive, Action<int> body)
+        {
+            var evt = new ManualResetEvent(false);
+
+            var count = toExclusive - fromInclusive;
+            var current = 0L;
+
+            for (var i = fromInclusive; i < toExclusive; ++i)
+            {
+                ThreadPool.QueueUserWorkItem((index) =>
+                {
+                    body((int)index);
+                    if (Interlocked.Increment(ref current) == count)
+                    {
+                        evt.Set();
+                    }
+                }, i);
+            }
+
+            evt.WaitOne();
+        }
+
+        public static void ForEach<TSource>(IEnumerable<TSource> source, Action<TSource> body)
+        {
+            var evt = new ManualResetEvent(false);
+
+            var last = source.Last();
+            foreach (var item in source)
+            {
+                ThreadPool.QueueUserWorkItem((itemObj) =>
+                {
+                    var sourceItem = (TSource)itemObj;
+                    body(sourceItem);
+                    if (sourceItem.Equals(last))
+                    {
+                        evt.Set();
+                    }
+                }, item);
+            }
+
+            evt.WaitOne();
+        }
+    }
+}
