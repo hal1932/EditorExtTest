0fcd19a5141a148dc63cb10c54a54846aacdd8db
hal1932 <yu.arai.19@gmail.com>
2016-05-13 03:33:10 +0900

    Http追加

---
+++ Assets/Editor/HttpTest.cs
@@ -0,0 +1,101 @@
+﻿using HlLib.Unity;
+using HlLib.Unity.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    public class HttpTest : ScriptableObject
+    {
+        [MenuItem("GameObject/test", false, 20)]
+        public static void Test()
+        {
+            var count = 0;
+            var begin = DateTime.Now;
+            ParallelUtil.For(0, 10000, (index) => count += f(index));
+            Debug.Log("1 " + (DateTime.Now - begin).Ticks);
+
+            count = 0;
+            begin = DateTime.Now;
+            ParallelUtil.ForEach(Enumerable.Range(0, 10000), (index) => count += f(index));
+            Debug.Log("3 " + (DateTime.Now - begin).Ticks);
+
+            //EditorUtility.DisplayProgressBar("", "", 0);
+            //Thread.Sleep(5000);
+            //EditorUtility.ClearProgressBar();
+
+#if false
+            using (var req = new HttpRequest("http://google.co.jp", HttpMethod.Get))
+            {
+                var res = req.Send();
+                using (var stream = res.GetStream())
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    Debug.Log(reader.ReadToEnd());
+                }
+            }
+
+            using (var req = new HttpRequest("http://google.co.jp", HttpMethod.Get))
+            {
+                var sendComplete = req.SendAsync(null, r => Debug.Log("hoge"));
+                sendComplete.WaitOne();
+                using (var stream = req.LastResponse.GetStream())
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    Debug.Log(reader.ReadToEnd());
+                }
+            }
+
+            using (var req = new HttpRequest("http://google.co.jp", HttpMethod.Get))
+            {
+                req.Timeout = 1;
+                var sendComplete = req.SendAsync(null, res =>
+                {
+                    if (res == null)
+                    {
+                        Debug.Log("timeout");
+                    }
+                });
+                sendComplete.WaitOne();
+            }
+#endif
+
+            using (var req = new HttpRequest("http://zipcloud.ibsnet.co.jp/api/search?zipcode=1000000", HttpMethod.Get))
+            {
+                var res = req.Send();
+                using (var stream = res.GetStream())
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    Debug.Log(reader.ReadToEnd());
+                }
+            }
+
+            using (var req = new HttpRequest("http://zipcloud.ibsnet.co.jp/api/search", HttpMethod.Post))
+            {
+                var param = "zipcode=1000000";
+                var data = Encoding.ASCII.GetBytes(param);
+
+                req.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+                req.Headers["Content-Length"] = data.Length.ToString();
+
+                var res = req.Send(data);
+                using (var stream = res.GetStream())
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    Debug.Log(reader.ReadToEnd());
+                }
+            }
+        }
+
+        private static int f(int i)
+        {
+            return i;
+        }
+    }
+}
---
+++ HlLib.Unity/Http/HttpResponse.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace HlLib.Unity.Http
+{
+    public class HttpResponse : IDisposable
+    {
+        public string ContentType { get { return _response.ContentType; } }
+        public long ContentLength { get { return _response.ContentLength; } }
+
+        internal HttpResponse(WebResponse response)
+        {
+            _response = response;
+        }
+
+        public Stream GetStream()
+        {
+            return _response.GetResponseStream();
+        }
+
+        #region IDisposable
+        ~HttpResponse()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            lock (_disposingLock)
+            {
+                if (_disposed)
+                {
+                    return;
+                }
+                _disposed = true;
+
+                if (_response != null)
+                {
+                    _response.Close();
+                    _response = null;
+                }
+            }
+        }
+
+        private bool _disposed;
+        private object _disposingLock = new object();
+        #endregion
+
+        private WebResponse _response;
+    }
+}
